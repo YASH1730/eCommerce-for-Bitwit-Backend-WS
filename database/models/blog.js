4c372121f3aa84f23f5629eb16c0b92aa9465671
Yashwant <yashwantsahu3002@gmail.com>
2022-05-28 20:01:04 +0530

    Blog Apis Seo parameters added

--- database/models/blog.js
+++ database/models/blog.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const user  = mongoose.Schema({
     uuid : {type : String,unique : true },
+    seo_title : String,
+    seo_description : String,
     title : String,
     card_image : String,
     card_description : String,
     description : String,
+    
 })
 
 module.exports = mongoose.model('blogData',user);
