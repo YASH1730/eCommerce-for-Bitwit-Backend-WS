4c67032d8bd588605b450306e924f8072d1acea5
Yashwant <yashwantsahu3002@gmail.com>
2022-08-10 18:02:23 +0530

    Drawer feild  added to product Task 25

--- database/models/products.js
+++ database/models/products.js
@@ -79,7 +79,9 @@ const product = mongoose.Schema({
     status : {required : true, type : Boolean},
     returnDays : {type : Number},
     COD : {type : Boolean},
-    returnable : {type : Boolean}
+    returnable : {type : Boolean},
+    drawer: {type : String},
+    drawer_count: {type : Number},
 })
 
 module.exports = mongoose.model('products',product);
